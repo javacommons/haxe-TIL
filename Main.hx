27825da2c97d52e85091b7b72ff683ddc3d989ab
javacommons <javacommons@gmail.com>
2019-10-17 13:58:44 +0900

    Add files via upload

--- Main.hx
+++ Main.hx
@@ -2,20 +2,44 @@ package;
 
 import neko.Lib;
 
-using Slambda;
-
-class Main 
+class Main
 {
-	
-	static function main() 
+
+	static function main()
 	{
 		trace("hello neko01");
 		var array = [1, 2, 3];
 		trace(array);
-		array = array.filter(x -> x % 2 == 0).map(x -> x * 10);
 		//array = array.filter(function (x) return x % 2 == 0).map(function (x) return x * 10);
+		array = array.filter(x -> x % 2 == 0).map(x -> x * 10);
 		trace(array);
-		Sys.getChar(true);
+		var ary2 = [for(i in 0...4) i * 2];
+		trace(ary2);
+		var a:Array<Any> =
+			//var a:Array<Dynamic> =
+			//var a:Dynamic =
+			[
+				[0, 1, 2],
+				[for(i in 0...3) i],
+				[
+					"ぜろ" => 0,
+					"いち" => 1,
+					"に" => 2
+				]
+			];
+		trace(a);
+		trace(a[2]);
+		$type(a[2]);
+		var b:Dynamic = a[2];
+		//var c = cast(b, Map<String, Int>);
+		var c:Map<String, Int> = b;
+		trace(c["に"]);
+		for (key => value in c)
+		{
+			trace(key, value);
+		}
+
+		//Sys.getChar(true);
 	}
-	
+
 }
